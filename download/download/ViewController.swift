fd73e4f52f700cdf5b94996a3bcfeaf2a14d6fa8
it180184 <f.ettinger@students.htl-leonding.ac.at>
2022-01-11 12:24:44 +0100

    download data

--- download/download/ViewController.swift
+++ download/download/ViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 class ViewController: UIViewController {
 
     var queue = DispatchQueue(label: "Download");
+    @IBOutlet weak var label: UILabel!
+    let path = "https://jsonplaceholder.typicode.com/todos";
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var variable = "Hallo";
+        let variable = "Hallo";
+        
         queue.async {
             let x = 420;
             print("in queue: \(variable)");
+            self.download()
+            
             DispatchQueue.main.async {
                 print("main \(x)")
+                self.label.text = "main \(x)"
+            }
+        }
+        
+    }
+    
+    func download() {
+        if let url = URL(string: path) {
+            if let data = try? Data(contentsOf: url) {
+                print("downloaded: \(data)");
+                if let obj = try? JSONSerialization.jsonObject(with: data, options: []) as? Array<Any>{
+                    print(obj);
+                } else {
+                    print("data parse failed")
+                }
+            } else {
+                print("download failed");
             }
         }
         

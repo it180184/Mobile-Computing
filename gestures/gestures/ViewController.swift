5a1143f587b7fc1415425e08ef976efb8fb7453f
it180184 <f.ettinger@students.htl-leonding.ac.at>
2022-02-01 12:36:11 +0100

    bingus herbert gaem

--- gestures/gestures/ViewController.swift
+++ gestures/gestures/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var bingus: UIImageView!
+    @IBOutlet weak var herbert: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,6 +24,10 @@ class ViewController: UIViewController {
           x: bingus.center.x + translation.x,
           y: bingus.center.y + translation.y
         )
+        if bingus.frame.intersects(herbert.frame) {
+            print("intersect");
+            herbert.image = UIImage.init(named: "herbert2.jpg")
+        }
 
         sender.setTranslation(.zero, in: view)
     }

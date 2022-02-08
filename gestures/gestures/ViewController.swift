4319d906d36d47b5d5698cdeb4d2ea8a6459f25d
it180184 <f.ettinger@students.htl-leonding.ac.at>
2022-02-08 12:21:39 +0100

    move bingos through constraints

--- gestures/gestures/ViewController.swift
+++ gestures/gestures/ViewController.swift
@@ -10,23 +10,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var bingus: UIImageView!
+    @IBOutlet weak var bingusLeft: NSLayoutConstraint!
+    @IBOutlet weak var bingusTop: NSLayoutConstraint!
+    
     @IBOutlet weak var herbert: UIImageView!
+    @IBOutlet weak var herbertLeft: NSLayoutConstraint!
+    @IBOutlet weak var herbertTop: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
+            timer in
+            
+            print("Timer was fired")
+        }
     }
 
     @IBAction func onPan(_ sender: UIPanGestureRecognizer) {
         let translation = sender.translation(in: view)
 
-
-        bingus.center = CGPoint(
+        bingusTop.constant += translation.y
+        bingusLeft.constant += translation.x
+        /*bingus.center = CGPoint(
           x: bingus.center.x + translation.x,
           y: bingus.center.y + translation.y
-        )
+        )*/
         if bingus.frame.intersects(herbert.frame) {
             print("intersect");
-            herbert.image = UIImage.init(named: "herbert2.jpg")
+            
         }
 
         sender.setTranslation(.zero, in: view)
